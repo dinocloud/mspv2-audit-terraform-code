144cdf721c034e6c034f222b6b862fe98f6d2017
ingdamiangonzalez <ing.damian.gonzalez@gmail.com>
2024-07-25 09:18:03 -0300

    feat: add security hub

--- modules/security_hub/main.tf
+++ modules/security_hub/main.tf
@@ -1,15 +1,12 @@
 ################# Security Hub AUDIT RESOURCES #################
 
-data "aws_caller_identity" "audit" {
-}
-
 resource "aws_securityhub_account" "audit" {
   enable_default_standards = false
 }
 
 # aggregators in audit
 resource "aws_securityhub_finding_aggregator" "this" {
-  linking_mode = "SPECIFIED_REGIONS"
+  linking_mode      = "SPECIFIED_REGIONS"
   specified_regions = var.specified_regions
-  depends_on   = [aws_securityhub_account.audit]
+  depends_on        = [aws_securityhub_account.audit]
 }
--- modules/security_hub/variables.tf
+++ modules/security_hub/variables.tf
@@ -2,5 +2,5 @@ variable "specified_regions" {
   description = "The regions to aggregate findings from."
   type        = list(string)
   default     = []
-  
+
 }
